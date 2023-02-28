05984cd30a4de853236150203217e18f6f1b6076
Harut <metsatunyanharut7@gmail.com>
2023-02-28 19:29:24 +0300

    blade features

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,26 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<x-layout>
+    @foreach($posts as $post)
 
-    <link href="./app.css" rel="stylesheet">
-    <script src="./app.js"></script>
-</head>
-<body>
-
-    <?php foreach($posts as $post): ?>
         <article>
             <h1>
-                <a href="/posts/<?=$post->slug?>">
-                    <?=$post->title;?>
-                    </a>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
+                </a>
             </h1>
-            <p><?=$post->body?></p>
+            {{ $post->excerpt }}
         </article>
-    <?php endforeach; ?>
+    @endforeach
+</x-layout>
 
-</body>
-</html>
