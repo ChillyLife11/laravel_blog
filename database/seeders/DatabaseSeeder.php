beb311c3614bd49c06d193295c74858812144c84
Harut <metsatunyanharut7@gmail.com>
2023-03-15 19:19:33 +0300

    Working with factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,46 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-
-        User::truncate();
-        Category::truncate();
-        
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'Harut'
         ]);
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
 
-//         \App\Models\User::factory()->create([
-//             'name' => 'Test User',
-//             'email' => 'test@example.com',
-//         ]);
+        User::factory(4)->create();
+        Post::factory(5)->create();
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => 'Lorem excerpt',
-            'body' => 'Lorem Body',
-        ]);
-
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => 'Lorem excerpt',
-            'body' => 'Lorem Body',
-        ]);
     }
 }
